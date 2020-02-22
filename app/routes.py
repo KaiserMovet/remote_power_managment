968e3938eb3e261325e3e5bb3e434153ce593760
KaiserMovet <rynkiewiczmate@gmail.com>
2020-02-22 17:22:03 +0100

    Moved wake up functionality to route

--- app/routes.py
+++ app/routes.py
@@ -2,20 +2,19 @@ from app import app as mapp
 from flask import render_template
 from app.engine.host_controller import HostController
 
-print(":fdfsfgsdfsdfdse")
-
 
 @mapp.route('/')
 @mapp.route('/index')
 def index():
-    lis = HostController().hosts
-    for li in lis:
-        li.wake_up()
+
     return render_template("base.html", ht=1)
 
 
 @mapp.route('/api/prussia/turn_on')
 def api_turn_on():
+    lis = HostController().hosts
+    for li in lis:
+        li.wake_up()
     print("TURNING ON")
     return render_template("base.html", ht=1)
 
